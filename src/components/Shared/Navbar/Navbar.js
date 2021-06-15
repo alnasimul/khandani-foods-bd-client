ef0948828154a24ae19fb36151cddbf036a4e47e
Al Nasimul Haque <alnasimul@gmail.com>
2021-06-15 23:23:50 +0600

    navbar setup done

--- src/components/Shared/Navbar/Navbar.js
+++ src/components/Shared/Navbar/Navbar.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import logo from '../../../images/logo.png';
 
 const Navbar = () => {
     return (
-        <div>
-            
+        <nav class="navbar navbar-expand-lg navbar-light .bg-white">
+        <div class="container-fluid">
+          <img src={logo} alt="" className="navbar-brand" style={{
+              width: '150px',
+              height: '150px'}}/>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse mx-5 " id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+              <li class="nav-item mx-3">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item mx-3">
+                <a class="nav-link" href="#">Features</a>
+              </li>
+              <li class="nav-item mx-3">
+                <a class="nav-link" href="#">Pricing</a>
+              </li>
+              <li class="nav-item mx-3">
+                <a class="nav-link btn btn-danger text-white px-2 py-2" href="#">Login</a>
+              </li>
+            </ul>
+          </div>
         </div>
+      </nav>
     );
 };
 
